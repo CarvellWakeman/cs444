e2c8a012919dbf050076a73f1f9a0318c37f12ed
Zach Lerew <lerewz@oregonstate.edu>
2017-09-30 14:25:46 -0700

    Initial directory and hw1 start

---
+++ homework1/hw1.c
@@ -0,0 +1,23 @@
+/*
+ * CS444 Homework #1 Concurrency #1 - The Producer-Consumer Problem
+ * Zach Lerew, Rohan Barve
+ * September 30, 2017
+*/
+
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+
+pthread_mutex_t mutex_lock;
+
+struct Buffer {
+    int val;
+    int time;
+};
+
+
+
+int main() {
+
+	return 0;
+}
